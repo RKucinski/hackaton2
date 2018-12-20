a75f11c5a10b77c28735e1ad01f5340fa552e07d
MorganCode <morgan.and.code@gmail.com>
2018-12-20 16:04:32 +0100

    component auth, signUp and bottombar

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,15 @@
+/* eslint-disable */
+
 import React, { Component } from 'react';
-import Home from './components/Home';
+import SignUp from './components/SignUp';
+// import Home from './components/Home';
+// import Auth from './components/Auth';
 
 class App extends Component {
   render() {
     return(
     <div >
-     <Home />
+  
     </div>
     )}
 }
