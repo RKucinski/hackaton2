03a74875e8615219bf65b2e021582c2d2ef3ff1d
Ysatis <place.ysatis@gmail.com>
2018-12-20 17:08:35 +0100

    WIP equipments page

---
+++ front/src/components/DetailEquipment.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Switch from '@material-ui/core/Switch';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+class componentName extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkedB: true,
+    };
+  }
+
+  handleChange = name => (event) => {
+    this.setState({ [name]: event.target.checked });
+  };
+
+  render() {
+    const { checkedB } = this.state;
+    return (
+      <div>
+        <ListItem button>
+          <ListItemText primary="Machine à laver" />
+        </ListItem>
+        <Switch
+          checked={checkedB}
+          onChange={this.handleChange('checkedB')}
+          value="checkedB"
+          color="primary"
+        />
+      </div>
+    );
+  }
+}
+
+export default componentName;
--- front/src/components/Equipments.jsx
+++ front/src/components/Equipments.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import DetailEquipment from './DetailEquipment';
 
-/*eslint-disable*/
+const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
 
 class Equipments extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
-    return <div />;
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <List component="nav">
+          <DetailEquipment />
+        </List>
+      </div>
+    );
   }
 }
 
-export default Equipments;
+Equipments.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Equipments);
