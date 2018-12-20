7deeddfe0672cc870c94c26fa1d5533c21ff3daf
MorganCode <morgan.and.code@gmail.com>
2018-12-20 16:47:32 +0100

    rename home

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,26 +2,27 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 /*eslint-disable*/
 
-import Accroche from './components/Accroche';
-import Stats from './components/Stats';
+// import Accroche from './components/Accroche';
+// import Stats from './components/Stats';
 import Equipments from './components/Equipments';
 import Auth from './components/Auth';
-import Signup from './components/Signup';
+import SignUp from './components/SignUp';
 import DetailEquipment from './components/DetailEquipment';
-import UserProfile from './components/UserProfile';
-import UserParameter from './components/UserParameter';
+// import UserProfile from './components/UserProfile';
+// import UserParameter from './components/UserParameter';
 
 export const App = () => (
   <div className="App">
     <Switch>
-      <Route exact path="/" component={Accroche} />
+      {/* <Route exact path="/" component={Accroche} /> */}
       <Route exact path="/auth" component={Auth} />
-      <Route exact path="/signup" component={Signup} />
-      <Route exact path="/stats" component={Stats} />
+      <Route exact path="/signUp" component={SignUp} />
+      {/* <Route exact path="/stats" component={Stats} /> */}
       <Route exact path="/equipments" component={Equipments} />
       <Route exact path="/equipments/detail" component={DetailEquipment} />
-      <Route exact path="/userprofile" component={UserProfile} />
-      <Route exact path="/userparameter" component={UserParameter} />
+      {/* <Route exact path="/userprofile" component={UserProfile} /> */}
+      {/* <Route exact path="/userparameter" component={UserParameter} /> */}
     </Switch>
   </div>
 );
+
