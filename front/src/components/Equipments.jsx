04093f413ad3f37da899f0bb1e466b354def299d
Ysatis <place.ysatis@gmail.com>
2018-12-20 15:16:18 +0100

    oopsy

---
+++ front/src/components/Equipments.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+/*eslint-disable*/
+
+class Equipments extends Component {
+  render() {
+    return <div>blabla</div>;
+  }
+}
+
+export default Equipments;
