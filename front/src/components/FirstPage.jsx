37a9ca40ccf3a5e29d8bafe9ca7e0b0d29b07a6e
Ysatis <place.ysatis@gmail.com>
2018-12-21 12:46:05 +0100

    Tips ramdon sur firstPage

--- front/src/components/FirstPage.jsx
+++ front/src/components/FirstPage.jsx
@@ -9,126 +9,149 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
 const styles = {
-	card: {
-		textAlign: 'center',
-		marginTop: 90,
-		maxWidth: 300,
-	},
-	cardTips: {
-		textAlign: 'center',
-		marginTop: 10,
-		maxWidth: 300,
-	},
-	media: {
-		height: 100,
-	},
-	cardContainerTips: {
-		marginTop: 35,
-		display: 'flex',
-		flexDirection: 'column',
-		maxWidth: 370,
-	},
-	link: {
-		textDecoration: 'none',
-	},
-	button: {
-		width: 100,
-		marginTop: 10,
-		marginBottom: 10,
-		backgroundColor: '#66cccc',
-	},
+  card: {
+    textAlign: 'center',
+    marginTop: 90,
+    maxWidth: 300,
+  },
+  cardTips: {
+    textAlign: 'center',
+    marginTop: 10,
+    maxWidth: 300,
+  },
+  media: {
+    height: 100,
+  },
+  cardContainerTips: {
+    marginTop: 35,
+    display: 'flex',
+    flexDirection: 'column',
+    maxWidth: 370,
+  },
+  link: {
+    textDecoration: 'none',
+  },
+  button: {
+    width: 100,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: '#66cccc',
+  },
 };
 
 class FirstPage extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			checkedMore: false,
-		};
-	}
-	handleMore = () => {
-		this.setState(state => ({ checkedMore: !state.checkedMore }));
-	};
-	render() {
-		const { classes } = this.props;
-		return (
-			<div>
-				{this.state.checkedMore ? null : (
-					<Card className={classes.card}>
-						<CardContent>
-							<Typography gutterBottom variant="h5" component="h2">
-								Impact Economique
-							</Typography>
-						</CardContent>
-						<CardMedia className={classes.media} image="https://via.placeholder.com/150" title="image" />
-						<br />
-						<Typography>
-							* legend
-							<br />
-							*legend
-						</Typography>
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkedMore: false,
+      tips: [
+        'Même en veille votre box Internet peut consommer jusqu’à 200kWh/an, pensez à l’éteindre et économisez jusque 30€ !',
+        'Optez pour des programmes en machine en cycle court vous permettra d’économiser entre 5 et 10€ par an !',
+        'Dégivrez votre réfrigérateur avant que la couche de givre n’atteigne 3mm vous permettra d’économiser jusqu’à 30% votre consommation d’électricité !',
+        `Le saviez-vous ? Une utilisation quotidienne de l'application HomEco vous permet d'économiser jusqu'à 7% sur votre facture d'électricité.`,
+        `85% des utilisateurs HomEco se sentent plus impliqués dans la sobriété numérique.`,
+      ],
+      selectedTips: '',
+    };
+  }
+  handleMore = () => {
+    this.setState(state => ({ checkedMore: !state.checkedMore }));
+  };
 
-						<Button
-							variant="contained"
-							color="primary"
-							onClick={() => this.handleMore()}
-							className={classes.button}
-						>
-							More
-						</Button>
+  componentDidMount = () => {
+    this.getRamdomTips();
+  };
 
-						<br />
-					</Card>
-				)}
-				{this.state.checkedMore && (
-					<Card className={classes.card}>
-						<CardContent>
-							<Typography gutterBottom variant="h5" component="h5">
-								Impact Economique
-							</Typography>
-						</CardContent>
-						<Typography>
-							L’économie réalisé en moyenne par un français utilisant TheShiftApp est de
-						</Typography>
-						<br />
-						<Typography>
-							* 763kW/h/an
-							<br />
-							* 110€/an
-							<br />
-							* 68,67 kg de CO2
-							<br />* Environ 2 arbres sauvés !
-						</Typography>
+  getRamdomTips = () => {
+    this.setState({
+      ...this.state,
+      selectedTips: this.state.tips[Math.floor(Math.random() * 4)],
+    });
+  };
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        {this.state.checkedMore ? null : (
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Impact Economique
+              </Typography>
+            </CardContent>
+            <CardMedia
+              className={classes.media}
+              image="https://via.placeholder.com/150"
+              title="image"
+            />
+            <br />
+            <Typography>
+              * legend
+              <br />
+              *legend
+            </Typography>
 
-						<Button
-							variant="contained"
-							color="primary"
-							onClick={() => this.handleMore()}
-							className={classes.button}
-						>
-							Back
-						</Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => this.handleMore()}
+              className={classes.button}
+            >
+              More
+            </Button>
 
-						<br />
-					</Card>
-				)}
-				<Card xs={12} sm={12} className={classes.cardContainerTips}>
-					<div className={classes.cardTips}>
-						<Typography component="h5" variant="h5">
-							Tips
-						</Typography>
-						<CardContent>
-							<Typography>Tips Random</Typography>
-						</CardContent>
-					</div>
-				</Card>
-			</div>
-		);
-	}
+            <br />
+          </Card>
+        )}
+        {this.state.checkedMore && (
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h5">
+                Impact Economique
+              </Typography>
+            </CardContent>
+            <Typography>
+              L’économie réalisé en moyenne par un français utilisant TheShiftApp est de
+            </Typography>
+            <br />
+            <Typography>
+              * 763kW/h/an
+              <br />
+              * 110€/an
+              <br />
+              * 68,67 kg de CO2
+              <br />* Environ 2 arbres sauvés !
+            </Typography>
+
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => this.handleMore()}
+              className={classes.button}
+            >
+              Back
+            </Button>
+
+            <br />
+          </Card>
+        )}
+        <Card xs={12} sm={12} className={classes.cardContainerTips}>
+          <div className={classes.cardTips}>
+            <Typography component="h5" variant="h5">
+              Tips
+            </Typography>
+            <CardContent>
+              <Typography>{this.state.selectedTips}</Typography>
+            </CardContent>
+          </div>
+        </Card>
+      </div>
+    );
+  }
 }
 
 FirstPage.propTypes = {
-	classes: PropTypes.shape({}).isRequired,
+  classes: PropTypes.shape({}).isRequired,
 };
 
 export default withStyles(styles)(FirstPage);
