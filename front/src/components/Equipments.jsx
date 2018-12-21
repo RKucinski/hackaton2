f1faab3f093af77aac56b98e81be999a7980fcd2
Ysatis <place.ysatis@gmail.com>
2018-12-21 11:08:39 +0100

    WIP Select type/marque/modele in EquipmentInfo

--- front/src/components/Equipments.jsx
+++ front/src/components/Equipments.jsx
@@ -3,82 +3,72 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { List, Fab, Switch } from '@material-ui/core';
+import { List, Fab } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import { Link } from 'react-router-dom';
-import QrCodeScan from './QrCodeScan';
 import DetailEquipment from './DetailEquipment';
 // import BottomNav from './BottomNav';
 import { withUser } from '../context/UserContext';
 
 const styles = theme => ({
-	root: {
-		width: '100%',
-		maxWidth: 360,
-		backgroundColor: theme.palette.background.paper,
-	},
-	fab: {
-		margin: theme.spacing.unit,
-		backgroundColor: '#66cccc',
-	},
-	extendedIcon: {
-		position: 'fixed',
-		top: 100,
-		marginRight: theme.spacing.unit,
-	},
-	button: {
-		position: 'fixed',
-		top: 190,
-		minWidth: 30,
-		backgroundColor: '#66cccc',
-	},
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  fab: {
+    margin: theme.spacing.unit,
+    backgroundColor: '#66cccc',
+  },
+  extendedIcon: {
+    position: 'fixed',
+    top: 100,
+    marginRight: theme.spacing.unit,
+  },
+  button: {
+    position: 'fixed',
+    top: 190,
+    minWidth: 30,
+    backgroundColor: '#66cccc',
+  },
 });
 
 class Equipments extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			checkedQrCode: false,
-			mapsList: [],
-		};
-	}
-	
-	handleChangeQrCode = () => {
-		this.setState(state => ({ checkedQrCode: !state.checkedQrCode }));
-		// mediaStream.stop();
-	};
-	displayEquipment = (itemsArray) => {
-    let array = []
-    itemsArray.map(item => {
-      array.push(<DetailEquipment item={item} />)
-    })
-    return array
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
 
+  displayEquipment = (itemsArray) => {
+    const array = [];
+    itemsArray.map((item) => {
+      array.push(<DetailEquipment item={item} />);
+    });
+    return array;
+  };
 
-	render() {
-		const { classes } = this.props;
-		const { checkedQrCode } = this.state;
-		return (
-			<div className={classes.root}>
-				<List component="nav">
-          {(this.props.userData.key === "nope") ? console.log('ternaire' + this.props.userData) : this.displayEquipment(this.props.userData.equipment)}
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <List component="nav">
+          {this.props.userData.key === 'nope'
+            ? console.log(`ternaire${this.props.userData}`)
+            : this.displayEquipment(this.props.userData.equipment)}
         </List>
         <Fab color="primary" aria-label="Add" className={classes.fab}>
           <Link to="/equipment/info">
             <AddIcon />
           </Link>
         </Fab>
-				<Switch checked={checkedQrCode} onChange={this.handleChangeQrCode} aria-label="Collapse" />
-				{this.state.checkedQrCode && <QrCodeScan />}
-			</div>
-		);
-	}
+      </div>
+    );
+  }
 }
 
 Equipments.propTypes = {
-	classes: PropTypes.shape({}).isRequired,
+  classes: PropTypes.shape({}).isRequired,
 };
 
 export default withUser(withStyles(styles)(Equipments));
-
